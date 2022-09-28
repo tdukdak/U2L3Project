7f15bbe3330053154f5f09cf9c5a3560a81137c2
tenzin <tdukdak9287@bths.edu>
2022-09-28 12:31:53 -0400

    Started adding code

--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -1,5 +1,10 @@
 public class U2L3Runner {
     public static void main(String[] args) {
+        Box box = new Box(10.5, 12.6, 15.75);
+        double boxVolume = box.volume();
+        System.out.println("My box has a volume of " + boxVolume);
+        System.out.println("My box has a volume of " + box.volume());
+        box.volume();
 
     }
 }
